3df9057132f84d6e2bcfc4fc5f92d560ddf3d9bb
zsfer <seiferalbacete@gmail.com>
2024-10-12 15:31:55 +0800

    feat: local multiplayer stuff

--- Assets/Scripts/Gameplay/GameManager.cs
+++ Assets/Scripts/Gameplay/GameManager.cs
@@ -1,22 +1,43 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class GameManager : MonoBehaviour
 {
-
     [field: SerializeField]
     public List<GameObject> Players { get; private set; }
+
+    [Header("References")]
+    [SerializeField] PlayerInputManager _inputManager;
 
     public static GameManager Instance { get; private set; }
     private void Awake()
     {
         Instance = this;
+
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnEnable()
     {
+        _inputManager.onPlayerJoined += OnPlayerJoined;
+        _inputManager.onPlayerLeft += OnPlayerLeft;
+    }
 
+    private void OnDisable()
+    {
+        _inputManager.onPlayerJoined -= OnPlayerJoined;
+        _inputManager.onPlayerLeft -= OnPlayerLeft;
+    }
+
+    private void OnPlayerLeft(PlayerInput input)
+    {
+        Players.Remove(input.gameObject);
+    }
+
+    private void OnPlayerJoined(PlayerInput input)
+    {
+        Players.Add(input.gameObject);
     }
 }
